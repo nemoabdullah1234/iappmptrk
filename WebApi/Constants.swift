09b7b2f8a18b11c49db19cd89cd8f8c2bb47b7f6
Your Name <nemoabdullah1234@gmail.com>
2018-08-27 10:15:25 +0530

    Initial Commit

---
+++ WebApi/Constants.swift
@@ -0,0 +1,57 @@
+//
+//  Constants.swift
+//  STRCourier
+//
+//  Created by Nitin Singh on 13/10/17.
+//  Copyright © 2017 OSSCube. All rights reserved.
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+//****************************** MQTT Constants ******************************//
+
+let CognitoIdentityPoolId = "us-west-2:7e0bd11b-0425-446e-94e3-3809f9fd3a2e"
+let CertificateSigningRequestCommonName = "Trakit"
+let CertificateSigningRequestCountryName = "IN"
+let CertificateSigningRequestOrganizationName = "akwa"
+let CertificateSigningRequestOrganizationalUnitName = "akwaproducts"
+let CognitoRegion = AWSRegionType.USWest2
+let PolicyName = "Akwa"
+let mqttTopic = "012179919676/us-west-2/ak_empTrackPoints"
+
+//****************************** AWS Cognito Constants ***********************//
+
+let projectIdConst = "us-west-2_XUUjby6xT"
+let clientIdConst = "012179919676"
+
+
+
+//****************************** Kontakt Key ********************************//
+
+let kontaktApiKey = "MdahnpkIOBvCxfOOmCKHtCdTaEXaqcSx"
+
+
+//****************************** Google Key ********************************//
+
+let googleApiKey = " AIzaSyBxLuuQSYUbK5kf9az8rs-sTfa4D-S8stQ"
+
+
+
+//****************************** Log File Key ********************************//
+
+
+let role = "EmpTrak"
+let roleLog = "Emp"
+
+//****************************** Cognito File Key ********************************//
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = .USWest2
+let CognitoIdentityUserPoolId = "us-west-2_XUUjby6xT"
+let CognitoIdentityUserPoolAppClientId = "7n2fb4dtlv2rqjg88aufu6ccqo"
+let CognitoIdentityUserPoolAppClientSecret = "pb95v6jlf6nk6flf3s1pecv7lqjtc94utoe0am0fshkkhs86nk3"
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"
+
+
+
+
